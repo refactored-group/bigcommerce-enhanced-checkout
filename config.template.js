2466ddfb8b899b890e3bfd3d086350177fb1812d
Andresa Martins <amartins@razoyo.com>
2025-03-12 17:52:48 -0300

    Adjust config generator to work with linux

--- config.template.js
+++ config.template.js
@@ -2,8 +2,9 @@
  * This file will be used to generate the public/js/config.js from your environment variables
  * FFL_STORE_ENDPOINT and FFL_IFRAME_URL.
  *
- * Run `npm run build` to generate the config.js file. The config file has also been added to .gitignore,
- * so you won't upload your configurations to the git repository
+ * Run `npm run generate-config` or `npm run build` to generate the config.js file.
+ * The config file has also been added to .gitignore, so you won't upload your configurations
+ * to the git repository
  *
  * @type {{FFL_IFRAME_URL: string, FFL_STORE_ENDPOINT: string}}
  */
